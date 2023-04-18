cffde1629515f218aa709ec03bc7851e60940967
raffi <raffi.hakhverdyan@spiritofsoft.com>
2023-04-18 16:14:42 +0400

    html to pdf

---
+++ src/main/java/rh/example/Converter.java
@@ -0,0 +1,57 @@
+package rh.example;
+
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.pdf.BaseFont;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Converter {
+
+    public static boolean convert(String htmlFilePath, String pdfDirPath) {
+        File html = new File(htmlFilePath);
+        File pdf = new File(pdfDirPath + "convertedPdf" + System.currentTimeMillis() + ".pdf");
+        try (FileOutputStream outputStream = new FileOutputStream(pdf)) {
+            ITextRenderer renderer = new ITextRenderer();
+            renderer.setDocument(html);
+            renderer.getFontResolver().addFont("src/resource/font/arnamu.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            renderer.getFontResolver().addFont("src/resource/font/arnamub.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            renderer.getFontResolver().addFont("src/resource/font/arnamubi.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            renderer.getFontResolver().addFont("src/resource/font/arnamui.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+
+
+            renderer.layout();
+            renderer.createPDF(outputStream);
+
+        } catch (IOException | DocumentException e) {
+            System.err.println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean convert(String htmlFilePath, String pdfDirPath, String pdfFileName) {
+        File html = new File(htmlFilePath);
+        File pdf = new File(pdfDirPath  + pdfFileName);
+        try (FileOutputStream outputStream = new FileOutputStream(pdf)) {
+            ITextRenderer renderer = new ITextRenderer();
+            renderer.setDocument(html);
+            renderer.getFontResolver().addFont("src/resource/font/arnamu.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            renderer.getFontResolver().addFont("src/resource/font/arnamub.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            renderer.getFontResolver().addFont("src/resource/font/arnamubi.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            renderer.getFontResolver().addFont("src/resource/font/arnamui.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+
+
+            renderer.layout();
+            renderer.createPDF(outputStream);
+
+        } catch (IOException | DocumentException e) {
+            System.err.println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+}
---
+++ src/main/java/rh/example/Main.java
@@ -0,0 +1,13 @@
+package rh.example;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        String htmlFilePath = "src/resource/html/htmlPage.html";
+        String pdfFilePath = "src/resource/pdf/";
+
+        Converter.convert(htmlFilePath, pdfFilePath);
+
+    }
+
+}
